be4584f8dcddeea365e56b0948509931d85349fa
Rhynia <65334131+RhNu@users.noreply.github.com>
2026-09-10 12:14:41 +0800

    fix(build): update browser bundle

--- tools/fixtures/script/index.ts
+++ tools/fixtures/script/index.ts
@@ -2,4 +2,8 @@ import './style.scss';
 import { clamp } from 'lodash';
 import { createRoot } from 'react-dom/client';
 
-console.info(clamp(2, 0, 1), createRoot);
+class ScriptFixture {
+  value = clamp(2, 0, 1);
+}
+
+console.info(new ScriptFixture().value, createRoot);
